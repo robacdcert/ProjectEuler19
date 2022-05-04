3e7ca0240b4dbd708759d068d65a4c3a468bae84
robacdcert <Robert.Griffin@IVP-LD6V08MG3>
2022-05-04 09:13:59 -0400

    Add project files.

---
+++ ProjectEuler19/Program.cs
@@ -0,0 +1,106 @@
+﻿//Project euler 19
+//How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+int day = 0;
+int weekday;
+string[] week = { "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday" };
+int sundays = 0;
+int year = 1901;
+
+while (year <= 2000)
+{
+    //Jan
+    if (week[(day + 1) % 7] == "Sunday")
+    {
+        sundays++;
+    }
+    day = day + 31;
+
+    //Feb
+    if (week[(day + 1) % 7] == "Sunday")
+    {
+        sundays++;
+    }
+    if (year % 4 == 0)
+    {
+        day = day + 29;
+    }
+    else
+    {
+        day = day + 28;
+    }
+
+    //March
+    if (week[(day + 1) % 7] == "Sunday")
+    {
+        sundays++;
+    }
+    day = day + 31;
+
+    //April
+    if (week[(day + 1) % 7] == "Sunday")
+    {
+        sundays++;
+    }
+    day = day + 30;
+
+    //May
+    if (week[(day + 1) % 7] == "Sunday")
+    {
+        sundays++;
+    }
+    day = day + 31;
+
+    //June
+    if (week[(day + 1) % 7] == "Sunday")
+    {
+        sundays++;
+    }
+    day = day + 30;
+
+    //July
+    if (week[(day + 1) % 7] == "Sunday")
+    {
+        sundays++;
+    }
+    day = day + 31;
+
+    //August
+    if (week[(day + 1) % 7] == "Sunday")
+    {
+        sundays++;
+    }
+    day = day + 31;
+
+    //September
+    if (week[(day + 1) % 7] == "Sunday")
+    {
+        sundays++;
+    }
+    day = day + 30;
+
+    //October
+    if (week[(day + 1) % 7] == "Sunday")
+    {
+        sundays++;
+    }
+    day = day + 31;
+
+    //November
+    if (week[(day + 1) % 7] == "Sunday")
+    {
+        sundays++;
+    }
+    day = day + 30;
+
+    //December
+    if (week[(day + 1) % 7] == "Sunday")
+    {
+        sundays++;
+    }
+    day = day + 31;
+
+    year++;
+}
+
+Console.WriteLine("Sundays: " + sundays);
